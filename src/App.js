a15efa63b0f3d13236905fcbf461f5302d56d21b
rimas12 <rizky_mashudy@yahoo.co.id>
2021-09-19 18:59:35 +0700

    push fetch api

--- src/App.js
+++ src/App.js
@@ -6,15 +6,32 @@ import Arrived from "./components/Arrived.js";
 import Client from "./components/Client.js";
 import AsideMenu from "./components/AsideMenu.js";
 import Footer from "./components/Footer.js";
+import { responsesAreSame } from 'workbox-broadcast-update';
 
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(function() {
+    (async function() {
+      const response = await fetch('https://prod-qore-app.qorebase.io/8ySrll0jkMkSJVk/allItems/rows?limit=7&offset=0&$order=asc', {
+        headers: {
+          "Content-Type": "application/json",
+          "accept": "application/json",
+          "x-api-key": process.env.REACT_APP_APIKEY
+        }
+      });
+      const { nodes } = await response.json();
+      setItems(nodes);
+    })();
+  }, []);
+
   return (
     <>
       <Header />
       <Hero />
       <Browse />
-      <Arrived />
+      <Arrived items={items}/>
       <Client />
       <AsideMenu />
       <Footer />
